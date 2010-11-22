6a7ceb1d753ef3fa54efb81df73296bacd060b23
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-22 12:42:10 +0000

    *ADD: -uptime fix

--- trunk/src/org/dh/usertrack/snmptest/Cisco.java
+++ trunk/src/org/dh/usertrack/snmptest/Cisco.java
@@ -55,5 +55,35 @@ public class Cisco {
 		}
 		return sIOS;
 	}
+
+	public static String getLocation(String sOID) {
+		
+		String sPattern1="1.3.6.1.2.1.1.6.0 =";
+		
+		sOID=sOID.replace(sPattern1, "");
+		
+		sOID=sOID.trim();
+		
+		return sOID;
+	}
+
+	public static String getUptime(String oid) {
+		oid=oid.replace("1.3.6.1.2.1.1.3.0 = ", "");
+		
+		if(oid.contains("day"))
+		{
+			oid=oid.substring(0, oid.indexOf("day")).trim()+":"+oid.substring(oid.indexOf(", ")+", ".length());
+		}else{
+			oid="0:"+oid;
+		}
+			
+		return oid;
+	}
+
+	public static String getAlias(String oid) {
+		oid=oid.replace("1.3.6.1.2.1.1.5.0 =","");
+		oid=oid.trim();
+		return oid;
+	}
 	
 }
