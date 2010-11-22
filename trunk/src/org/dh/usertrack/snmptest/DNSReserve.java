dcd35af3ea1e5cd1e2579d0205504a213cea3e4f
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-22 12:13:13 +0000

    *ADD: -DNSReserve method

--- trunk/src/org/dh/usertrack/snmptest/DNSReserve.java
+++ trunk/src/org/dh/usertrack/snmptest/DNSReserve.java
@@ -1,5 +1,68 @@
 package org.dh.usertrack.snmptest;
 
+import java.net.InetAddress;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
 public class DNSReserve {
 
+	public static final String lookupDNS(String IP){
+		
+		String sDNS="";
+		
+		try{
+		
+		InetAddress adr = InetAddress.getByName(IP) ;
+	      byte[] addr = adr.getAddress() ;
+
+	      // build reverse IP address
+	      String addrString = "";
+	      for (int i=addr.length-1 ; i>=0; i--)
+	      { addrString += (addr[i] & 0xFF) + "."    ;
+	      }	
+			
+		Hashtable env = new Hashtable();
+
+		env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+		env.put("java.naming.provider.url",    "dns://151.10.136.202/. dns://151.10.136.70/.");
+		
+		
+		DirContext ctx = new InitialDirContext(env);
+
+		Attributes attrs = ctx.getAttributes(addrString+".in-addr.arpa",new String[] {"PTR"});
+
+		for (NamingEnumeration ae = attrs.getAll();ae.hasMoreElements();) {
+
+			Attribute attr = (Attribute)ae.next();
+
+			String attrId = attr.getID();
+
+			for (Enumeration vals = attr.getAll();vals.hasMoreElements();)
+			{
+			//System.out.println(attrId + ": " + vals.nextElement());
+				sDNS=(String) vals.nextElement();
+			}
+			
+		}
+
+		ctx.close();
+
+ 	}	
+
+	catch(Exception e) {
+
+		System.out.println("ERROR: NO REVERSE DNS");
+
+	}
+		
+		return "";
+		
+	}
+	
 }
