18cd8f863446d483df527a10df5baefbb1379dfe
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-22 08:47:34 +0000

    *ADD: -get Model function works on all available cisco switchs

--- trunk/src/org/dh/usertrack/snmptest/Cisco.java
+++ trunk/src/org/dh/usertrack/snmptest/Cisco.java
@@ -3,7 +3,44 @@ package org.dh.usertrack.snmptest;
 public class Cisco {
 
 	public static final String getModelfromDescr(String sOID){
-		return "";
+		
+		String sModell="";
+		String sIOSTM="IOS (tm)";
+		String sPatternS="Software (";
+		String sPatternE="), Version";
+		String sPattern1="1.3.6.1.2.1.1.1.0 = Cisco IOS Software,";
+		String sPattern2="1.3.6.1.2.1.1.1.0 = Cisco Internetwork Operating System Software";
+		String sPattern3="1.3.6.1.2.1.1.1.0 = Cisco Systems ";
+		String sPattern4="Cisco Catalyst Operating System Software";
+		
+		if(sOID.contains(sPattern1))
+		{
+			sModell=sOID.substring(sOID.indexOf(sPatternS)+sPatternS.length(),sOID.indexOf(sPatternE));
+			sModell=sModell.trim();
+		}else if(sOID.contains(sPattern2)){
+			sModell=sOID.substring(sOID.indexOf(sPatternS)+sPatternS.length(),sOID.indexOf(sPatternE));
+			sModell=sModell.replace(sIOSTM, "");
+			sModell=sModell.trim();
+		
+		}else if(sOID.contains(sPattern3))
+		{	
+			sModell=sOID.substring(sOID.indexOf(sPattern3)+sPattern3.length(), sOID.indexOf(sPattern4));
+		}else{
+			System.out.println(sOID);
+		}
+		
+		if(sModell.contains("C6MSFC2-JSV-M"))
+		{
+			sModell="C6500-MSFC2-JSV-M";
+		}
+		
+		return sModell;
+	}
+	
+	public static final String getIOSfromDescr(String sOID){
+		String sIOS="";
+				
+		return sIOS;
 	}
 	
 }
