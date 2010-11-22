4d0a72e6e3e41621a7a670601009a4bed794de4a
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-22 12:42:32 +0000

    *FIX: -dns functions

--- trunk/src/org/dh/usertrack/snmptest/DNSHelperClass.java
+++ trunk/src/org/dh/usertrack/snmptest/DNSHelperClass.java
@@ -5,14 +5,20 @@ public class DNSHelperClass {
 	public static final String getHostname(String IP){
 		String sHostname="";
 		sHostname=DNSReserve.lookupDNS(IP);
+		if(sHostname.length()>0)
+		{
 		sHostname=sHostname.substring(0,sHostname.indexOf("."));
+		}
 		return sHostname;
 	}
 	
 	public static final String getDomainname(String IP){
 		String sDomainname="";
 		sDomainname=DNSReserve.lookupDNS(IP);
+		if(sDomainname.length()>0)
+		{
 		sDomainname=sDomainname.substring(sDomainname.indexOf("."));
+		}
 		return sDomainname;
 	}
 	
