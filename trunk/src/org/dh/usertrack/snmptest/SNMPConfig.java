09532bd711af8ebea255b9951bdba57becd2f5bb
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-22 08:46:58 +0000

    *ADD: -added Thread count to config

--- trunk/src/org/dh/usertrack/snmptest/SNMPConfig.java
+++ trunk/src/org/dh/usertrack/snmptest/SNMPConfig.java
@@ -2,6 +2,14 @@ package org.dh.usertrack.snmptest;
 
 public class SNMPConfig {
 
+	public static final int getThreadmaxcount(){
+		
+		return 4;
+		
+		//return HelperClass.getCPUCount()*2;
+		
+	}
+	
 	public static final String getReadCommunity(){
 		
 		return "pdhoechst";
