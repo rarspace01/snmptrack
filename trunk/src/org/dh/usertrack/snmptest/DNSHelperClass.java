94e6b0bd23c3617b396748b8b7f4122ebb203d9f
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-23 12:04:53 +0000

    *FIX: -no trimming

--- trunk/src/org/dh/usertrack/snmptest/DNSHelperClass.java
+++ trunk/src/org/dh/usertrack/snmptest/DNSHelperClass.java
@@ -5,22 +5,22 @@ public class DNSHelperClass {
 	public static final String getHostname(String IP){
 		String sHostname="";
 		sHostname=DNSReserve.lookupDNS(IP);
-		if(sHostname.length()>0&&!sHostname.contains("fritz."))
-		{
-		sHostname=sHostname.substring(0,sHostname.indexOf("."));
-		}
-		sHostname=sHostname.substring(0, sHostname.lastIndexOf("."));
+//		if(sHostname.length()>0&&!sHostname.contains("fritz."))
+//		{
+//		sHostname=sHostname.substring(0,sHostname.indexOf("."));
+//		//sHostname=sHostname.substring(0, sHostname.lastIndexOf("."));
+//		}
 		return sHostname;
 	}
 	
 	public static final String getDomainname(String IP){
 		String sDomainname="";
 		sDomainname=DNSReserve.lookupDNS(IP);
-		if(sDomainname.length()>0&&!sDomainname.contains("fritz."))
-		{
-		sDomainname=sDomainname.substring(sDomainname.indexOf("."));
-		}
-		sDomainname=sDomainname.substring(0, sDomainname.lastIndexOf(","));
+//		if(sDomainname.length()>0&&!sDomainname.contains("fritz."))
+//		{
+//		sDomainname=sDomainname.substring(sDomainname.indexOf("."));
+//		//sDomainname=sDomainname.substring(0, sDomainname.lastIndexOf(","));
+//		}
 		return sDomainname;
 	}
 	
