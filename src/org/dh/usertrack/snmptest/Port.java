80faf247a2439d24259237bb6c5f4e3dec166c7a
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-23 15:54:07 +0000

    *ADD: -vardump aequivalent

--- src/org/dh/usertrack/snmptest/Port.java
+++ src/org/dh/usertrack/snmptest/Port.java
@@ -1,7 +1,7 @@
 package org.dh.usertrack.snmptest;
 
 public class Port {
-	public String sMAC="";
+	public String sMAC;
 	public String SwitchIP;
 	public int PortID;
 	public int VPortID;
@@ -12,5 +12,30 @@ public class Port {
 	public boolean cstatus;
 	public String Speed;
 	public String Duplex;
+	public boolean isUplink;
+	public String UplinkIP;
+	
+	public Port() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String printAll() {
+		// TODO Auto-generated method stub
+		return sMAC+" "+
+		SwitchIP+" "+
+		PortID+" "+
+		VPortID+" "+
+		alias+" "+
+		vlan+" "+
+		LastUpdate+" "+
+		astatus+" "+
+		cstatus+" "+
+		Speed+" "+
+		Duplex+" "+
+		isUplink+" "+
+		UplinkIP+" ";
+	}
+	
+	
 	
 }
