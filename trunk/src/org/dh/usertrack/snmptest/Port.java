e2eeec6f1961c7b244cfe908a8528ba3c6d3e0e1
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-23 13:30:38 +0000

    *ADD: -Port class

---
+++ trunk/src/org/dh/usertrack/snmptest/Port.java
@@ -0,0 +1,16 @@
+package org.dh.usertrack.snmptest;
+
+public class Port {
+	public String sMAC="";
+	public String SwitchIP;
+	public int PortID;
+	public int VPortID;
+	public String alias;
+	public int vlan;
+	public long LastUpdate;
+	public boolean astatus;
+	public boolean cstatus;
+	public String Speed;
+	public String Duplex;
+	
+}
