61935cb42979194c84c8326aefe5fbf47241428d
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-24 09:45:34 +0000

    *ADD: -finished port logic

--- src/org/dh/usertrack/Switch.java
+++ src/org/dh/usertrack/Switch.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import org.snmp4j.Snmp;
+import org.snmp4j.smi.IpAddress;
 
 public class Switch {
 
@@ -52,6 +53,8 @@ public class Switch {
 	String sSQL="";	
 	String sPuffer="";
 	
+	boolean supportsCiscoDuplex=false;
+	
 	//Get switch info	
 		
 		
@@ -87,7 +90,7 @@ public class Switch {
 		svendor="Unknown"+"["+sOID+"]";
 	}
 	
-	HelperClass.msgLog("["+sIP+"]DNS:["+sDNS+"] Vendor: ["+svendor+"] Model: ["+smodel+"] IOS: ["+sversion+"] LOC: ["+sLocation+"] Uptime: ["+iUptime+"] Alias: ["+sAlias+"]");
+	//HelperClass.msgLog("["+sIP+"]DNS:["+sDNS+"] Vendor: ["+svendor+"] Model: ["+smodel+"] IOS: ["+sversion+"] LOC: ["+sLocation+"] Uptime: ["+iUptime+"] Alias: ["+sAlias+"]");
 
 	iTimestamp=(int)(System.currentTimeMillis()/1000);
 	
@@ -126,6 +129,11 @@ public class Switch {
 	ArrayList<String> swHostMAC=new ArrayList<String>(); //Liste der Mac Adressen
 	ArrayList<String> swCDP=new ArrayList<String>();
 	ArrayList<String> swVLANListe=new ArrayList<String>();
+	ArrayList<String> swSpeed=new ArrayList<String>();
+	//only for selected cisco switchs
+	ArrayList<String> swCiscoPort=new ArrayList<String>();
+	ArrayList<String> swCiscoDuplex=new ArrayList<String>();	
+	ArrayList<String> swDuplex=new ArrayList<String>();
 	
 	swMACs=SNMPHandler.getOIDWalk(snmp, "1.3.6.1.2.1.2.2.1.6", sIP, SNMPConfig.getReadCommunity());
 	swStatus=SNMPHandler.getOIDWalk(snmp, "1.3.6.1.2.1.2.2.1.8", sIP, SNMPConfig.getReadCommunity());
@@ -134,6 +142,25 @@ public class Switch {
 	swPortalias=SNMPHandler.getOIDWalk(snmp, "1.3.6.1.2.1.31.1.1.1.18", sIP, SNMPConfig.getReadCommunity());
 	swVPort=SNMPHandler.getOIDWalk(snmp, "1.3.6.1.2.1.17.1.4.1.2", sIP, SNMPConfig.getReadCommunity());
 	swCDP=SNMPHandler.getOIDWalk(snmp, "1.3.6.1.4.1.9.9.23.1.2.1.1.4", sIP, SNMPConfig.getReadCommunity());
+	swSpeed=SNMPHandler.getOIDWalk(snmp, "1.3.6.1.2.1.2.2.1.5", sIP, SNMPConfig.getReadCommunity());
+	swCiscoPort = SNMPHandler.getOIDWalk(snmp, "1.3.6.1.4.1.9.9.87.1.4.1.1.25", sIP, SNMPConfig.getReadCommunity());
+	
+	if(swCiscoPort.size()>0){
+	swCiscoDuplex = SNMPHandler.getOIDWalk(snmp, "1.3.6.1.4.1.9.9.87.1.4.1.1.32", sIP, SNMPConfig.getReadCommunity());
+	supportsCiscoDuplex=true;
+	
+	for(int i=0;i<swCiscoPort.size()||i<swCiscoDuplex.size();i++)
+	{
+	
+		swDuplex.add(swCiscoPort.get(i).substring(swCiscoPort.get(i).indexOf("!")+1)
+				+"!"+swCiscoDuplex.get(i).substring(swCiscoDuplex.get(i).indexOf("!")+1)
+				);
+		
+	}
+	
+	
+	
+	}
 	
 	//get all the macs of the coresponding vlans
 	
@@ -150,9 +177,7 @@ public class Switch {
 		swPuffer.clear();
 	}
 	
-	System.out.println("DEBUG: swMAC Size:"+swHostMAC.size());
-	
-	
+	//System.out.println("DEBUG: swMAC Size:"+swHostMAC.size());
 	
 	if(swMACs.size()==0){
 		System.out.println("ERROR swMAClist");
@@ -169,14 +194,60 @@ public class Switch {
 				p.sMAC=sPuffer;
 				p.SwitchIP=sIP;
 				p.PortID=Integer.parseInt(swMACs.get(i).substring(0,swMACs.get(i).indexOf("!")).replace("1.3.6.1.2.1.2.2.1.6.", ""));
+				p.name=getOIDListEntry(swPortname,i+1).substring(getOIDListEntry(swPortname,i+1).indexOf("!")+1);
+				p.alias=getOIDListEntry(swPortalias,i+1).substring(getOIDListEntry(swPortalias,i+1).indexOf("!")+1);
+				p.vlan=getOIDListEntry(swVLANs,i+1).substring(getOIDListEntry(swVLANs,i+1).indexOf("!")+1);
 				
-				System.out.println(p.printAll());
+				//get cstatus of port
+				
+				sPuffer=getOIDListEntry(swStatus, i+1).substring(getOIDListEntry(swStatus, i+1).indexOf("!")+1);
+				
+				if(sPuffer.contains("1"))
+				{
+				p.cstatus=true;
+				
+				p.Speed=getOIDListEntry(swSpeed, i+1).substring(getOIDListEntry(swSpeed, i+1).indexOf("!")+1);
+				
+				}else{
+				p.cstatus=false;	
+				
+				p.Speed="0";
+				}
+			
+				//Duplex Erkennung - nur für spezielle Cisco Switchs möglich
+				
+				if(supportsCiscoDuplex)
+				{
+					sPuffer=getDuplexSTate(swDuplex,p.PortID).substring(sPuffer.indexOf("!")+1);
+					
+					if(sPuffer.contains("2")&&p.cstatus)
+					{
+					p.Duplex="HalfDuplex";
+					}else if(sPuffer.contains("1")&&p.cstatus)
+					{
+					p.Duplex="FullDuplex";
+					}else{
+						p.Duplex="";
+					}
+					sPuffer="";
+				}
+				
+				//Prüfe ob Uplink Port + Setzte Uplink IP sofern möglich
+				
+				if(isUplinkport(swCDP, p.PortID, p.vlan))
+				{
+					p.isUplink=true;
+					p.UplinkIP=getUplinkIP(swCDP, p.PortID, p.vlan);
+				}
+				//
+				
+				//System.out.println(p.printAll());
 				
 			
 				
 				
 				try {
-					Thread.sleep(500);
+					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -206,6 +277,48 @@ public class Switch {
 	
 	}
 	
+	private String getUplinkIP(ArrayList<String> swCDP, int portID, String vlan) {
+		String sPuffer="";
+		
+		for(int i=0;i<swCDP.size();i++){
+
+			if(swCDP.get(i).contains("1.3.6.1.4.1.9.9.23.1.2.1.1.4."+portID)&&vlan.length()==0)
+			{
+			sPuffer=swCDP.get(i).substring(swCDP.get(i).indexOf("!")+1);
+			}
+		}
+		
+		sPuffer=HexToDec.getDec(sPuffer);
+		
+		return sPuffer;
+	}
+
+	private boolean isUplinkport(ArrayList<String> swCDP, int portID, String vlan) {
+		boolean isUplink=false;
+		
+		for(int i=0;i<swCDP.size();i++){
+
+			if(swCDP.get(i).contains("1.3.6.1.4.1.9.9.23.1.2.1.1.4."+portID)&&vlan.length()==0)
+			{
+			isUplink=true;
+			}
+		}
+		
+		return isUplink;
+	}
+
+	private String getDuplexSTate(ArrayList<String> swDuplex, int portID) {
+		String sPuffer="";
+		
+		for (int i=0;i<swDuplex.size(); i++)
+		{
+			if(swDuplex.get(i).contains(""+portID)){
+				sPuffer=swDuplex.get(i);
+			}
+		}
+		return sPuffer;
+	}
+
 	public ArrayList<String> getVLanList(ArrayList<String> Liste){
 		String sTMP="";
 		ArrayList<String> vll=new ArrayList<String>();
@@ -222,6 +335,18 @@ public class Switch {
 		}
 		Collections.sort(vll);
 		return vll;
+	}
+	
+	public String getOIDListEntry(ArrayList<String> swListe, int s){
+		String sPuffer="";
+		for(int i=0;i<swListe.size();i++)
+		{
+			if(swListe.get(i).contains("."+s+"!"))
+			{
+				sPuffer=swListe.get(i);
+			}
+		}
+		return sPuffer;
 	}
 	
 }
