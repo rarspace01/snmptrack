767879f8932a60377176c547a72bbf3e05054d16
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-24 18:27:29 +0000

    *ADD: -Host class get DB string updated

--- trunk/src/org/dh/usertrack/Host.java
+++ trunk/src/org/dh/usertrack/Host.java
@@ -10,4 +10,28 @@ public class Host {
 	public String Speed;
 	public String Duplex;
 	
+	public String getDBString() {
+		
+
+		
+		String sSQL="";
+		
+		int iTimestamp=(int)(System.currentTimeMillis()/1000);
+		
+		sSQL="MERGE INTO USRTRACK.\"st_hosts\" dest "+
+		"USING dual ON (dual.dummy is not null and dest.MAC='"+MAC+"' and dest.PortMAC="+PortMAC+") "+
+		"WHEN MATCHED "+
+		" THEN UPDATE SET "+
+		"\"IP\"='"+IP+"',"+
+		"\"hostname\"='"+hostname+"',"+
+		"\"stamptime\"='"+iTimestamp+"',"+
+		"\"lastuser\"='"+lastuser+"',"+
+		"\"Speed\"='"+Speed+"',"+
+		"\"Duplex\"='"+Duplex+"' "+
+		"WHEN NOT MATCHED "+
+		 "THEN INSERT (\"MAC\",\"PortMAC\",\"IP\",\"hostname\",\"stamptime\",\"lastuser\",\"Speed\",\"Duplex\") VALUES ('"+MAC+"',"+PortMAC+",'"+IP+"','"+hostname+"','"+iTimestamp+"','"+lastuser+"','"+Speed+"','"+Duplex+"')";
+
+		return sSQL;
+	}
+	
 }
