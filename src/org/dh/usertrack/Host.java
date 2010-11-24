c9f6630caa8a78fe5292e5c1ce4988dcba1b0d2d
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-24 18:15:21 +0000

    *ADD: -added host class

---
+++ src/org/dh/usertrack/Host.java
@@ -0,0 +1,13 @@
+package org.dh.usertrack;
+
+public class Host {
+
+	public String MAC;
+	public String PortMAC;
+	public String IP;
+	public String hostname;
+	public String lastuser;
+	public String Speed;
+	public String Duplex;
+	
+}
