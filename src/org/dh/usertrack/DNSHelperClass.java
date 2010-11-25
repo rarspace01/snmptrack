e955ad6ac84fc8257fe74c76da73b9ad24965269
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-25 08:25:59 +0000

    *ADD: -select DNS server on DNS reserve lookup

--- src/org/dh/usertrack/DNSHelperClass.java
+++ src/org/dh/usertrack/DNSHelperClass.java
@@ -4,25 +4,17 @@ public class DNSHelperClass {
 
 	public static final String getHostname(String IP){
 		String sHostname="";
-		sHostname=DNSReserve.lookupDNS(IP);
-//		if(sHostname.length()>0&&!sHostname.contains("fritz."))
-//		{
-//		sHostname=sHostname.substring(0,sHostname.indexOf("."));
-//		//sHostname=sHostname.substring(0, sHostname.lastIndexOf("."));
-//		}
+		sHostname=DNSReserve.lookupDNS("151.10.136.202",IP);
+		if(sHostname.contains("NODNS"))
+		{
+			sHostname=DNSReserve.lookupDNS("151.10.136.70",IP);	
+		}
+		
+		if(sHostname.contains("NODNS")){
+			sHostname="";
+		}
+		
 		return sHostname;
 	}
-	
-	public static final String getDomainname(String IP){
-		String sDomainname="";
-		sDomainname=DNSReserve.lookupDNS(IP);
-//		if(sDomainname.length()>0&&!sDomainname.contains("fritz."))
-//		{
-//		sDomainname=sDomainname.substring(sDomainname.indexOf("."));
-//		//sDomainname=sDomainname.substring(0, sDomainname.lastIndexOf(","));
-//		}
-		return sDomainname;
-	}
-	
 	
 }
