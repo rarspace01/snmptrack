0779acb849b28dfcf8b6e6a07f6ae2790447030d
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-25 12:43:07 +0000

    *FIX: -improved DNS server mgt

--- trunk/src/org/dh/usertrack/DNSHelperClass.java
+++ trunk/src/org/dh/usertrack/DNSHelperClass.java
@@ -1,20 +1,38 @@
 package org.dh.usertrack;
 
+import java.util.ArrayList;
+
 public class DNSHelperClass {
 
 	public static final String getHostname(String IP){
-		String sHostname="";
-		sHostname=DNSReserve.lookupDNS("151.10.136.202",IP);
-		if(sHostname.contains("NODNS"))
+		ArrayList<String> DNSServer=new ArrayList<String>();
+		String sPuffer="NODNS";
+		int i=0;
+		
+		DNSServer.add("151.10.136.202");
+		DNSServer.add("151.10.136.70");
+		long time1=System.currentTimeMillis();
+		
+		while(sPuffer.contains("NODNS")&&i<DNSServer.size())
 		{
-			sHostname=DNSReserve.lookupDNS("151.10.136.70",IP);	
+			
+			sPuffer=DNSReserve.lookupDNS(DNSServer.get(i),IP);
+			i++;
 		}
 		
-		if(sHostname.contains("NODNS")){
-			sHostname="";
+		long time2=System.currentTimeMillis();
+		
+		if((time2-time1)>90)
+		{
+		System.out.println("DNS resovle took "+(time2-time1)+"ms for H["+IP+"]");
+		}
+
+		if(sPuffer.contains("NODNS"))
+		{
+			sPuffer="";
 		}
 		
-		return sHostname;
+		return sPuffer;
 	}
 	
 }
