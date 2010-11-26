03ee15a98844c1015cdeb83bf7e262cc24d1252a
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-26 14:51:41 +0000

    *ADD: -read from DB if switchs.xml  not existent

--- trunk/src/org/dh/usertrack/SwitchListe.java
+++ trunk/src/org/dh/usertrack/SwitchListe.java
@@ -21,49 +21,70 @@ public class SwitchListe {
 		
 		File file = new File("switchs.xml");
 		
+		if(file.exists())
+		{
+		HelperClass.msgLog("Found switch.xml");	
+		
 		FileInputStream fis = null;
-	    BufferedInputStream bis = null;
-	    BufferedReader br= null;
-
-	    try {
-	      fis = new FileInputStream(file);
-
-	      // Here BufferedInputStream is added for fast reading.
-	      bis = new BufferedInputStream(fis);
-	      br = new BufferedReader(new InputStreamReader(bis));
-
-	      String sLine="";
-	      
-	      // dis.available() returns 0 if the file does not have more lines.
-	      while ((sLine = br.readLine()) != null) {
-
-	      // this statement reads the line from the file and print it to
-	        // the console.
-	        
-	        String sStart="<switch ip=\"";
-	        String sEnde="\"/>";
-	        
-	        if(sLine.contains(sStart)&&sLine.contains(sEnde)){ 
-	        	
-	        addsw(sLine.substring(sLine.indexOf(sStart)+sStart.length(), sLine.indexOf(sEnde)));
-	        	
-	      }
-
-	        
-	        
-	        
-	      }
-	        
-	      // dispose all the resources after using them.
-	      fis.close();
-	      bis.close();
-	      br.close();
-
-	    } catch (FileNotFoundException e) {
-	      e.printStackTrace();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }
+		BufferedInputStream bis = null;
+		BufferedReader br= null;
+		
+		try {
+			fis = new FileInputStream(file);
+			
+			// Here BufferedInputStream is added for fast reading.
+			bis = new BufferedInputStream(fis);
+			br = new BufferedReader(new InputStreamReader(bis));
+			
+			String sLine="";
+			
+			// dis.available() returns 0 if the file does not have more lines.
+			while ((sLine = br.readLine()) != null) {
+				
+				// this statement reads the line from the file and print it to
+				// the console.
+				
+				String sStart="<switch ip=\"";
+				String sEnde="\"/>";
+				
+				if(sLine.contains(sStart)&&sLine.contains(sEnde)){ 
+					
+					addsw(sLine.substring(sLine.indexOf(sStart)+sStart.length(), sLine.indexOf(sEnde)));
+					
+				}
+				
+				
+				
+				
+			}
+			
+			// dispose all the resources after using them.
+			fis.close();
+			bis.close();
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		}else{
+		String sPuffer="";	
+		HelperClass.msgLog("No switch.xml found, reading from nagios");	
+		
+		sPuffer=HttpHelperClass.getPage("");
+		
+		System.out.println(sPuffer);
+		
+		try {
+			Thread.sleep(9999);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		}
+		
 		
 		
 	}
