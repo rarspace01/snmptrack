9edb0e3dd9272e5eb3c4ab871deb12d6d3fcb5ff
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-26 13:39:30 +0000

    *ADD: -1 phase commits

--- src/org/dh/usertrack/DataManagerOracleMulti.java
+++ src/org/dh/usertrack/DataManagerOracleMulti.java
@@ -6,96 +6,50 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 
 /**Die Klasse DataManager dient zum Verwalten der Verbindungen mit der Datenbank*/
 public class DataManagerOracleMulti {
-        private static DataManagerOracleMulti uniqueInstance = null;
-        
-        private static long lLastAccessed=0;
-        
-        private java.sql.Connection conn;
-        private Statement stmt;
-        private PreparedStatement pstmt;
-        
-        private DataManagerOracleMulti() {
-                try {
-                        Class.forName("oracle.jdbc.driver.OracleDriver"); 
-                        conn = DriverManager.getConnection("jdbc:oracle:thin:@octopus:1521:USRTRACK","USRTRACK","TrackIt");
-                        conn.setAutoCommit(false);
-                        //stmt = conn.createStatement();
-                        
-                        
-                } catch (SQLException e) {
-                        
-                        e.printStackTrace();
-                        HelperClass.err(e);
-                } catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-                uniqueInstance = this;
-        }
 
-        public static DataManagerOracleMulti getInstance()
-        {
-                        if(uniqueInstance== null||Calendar.getInstance().getTimeInMillis()>(lLastAccessed+(999*1000)))
-                        {
-                    	uniqueInstance=null;
-                        new DataManagerOracleMulti();
-                        }
-                        lLastAccessed=Calendar.getInstance().getTimeInMillis();
-                return uniqueInstance;  
-        }
-        
-        public void dispose()
-        {
-                uniqueInstance=null;
-        }
-        
-        //Methode für normale Select Operationen
-        public ResultSet select(String SQLString) throws SQLException
-        {
-                ResultSet rs=null;
+		public static final void execute(ArrayList<String> sSQLList){
+			
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				
+				Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@octopus:1521:USRTRACK","USRTRACK","TrackIt");
+	            conn.setAutoCommit(false);
+	        	
+	        	ArrayList<PreparedStatement> psL=new ArrayList<PreparedStatement>();
+	        	
+	        	for(int i=0;i<sSQLList.size();i++)
+	        	{
+	        		psL.add(conn.prepareStatement(sSQLList.get(i)));
+	        	}
+	        	
+	        	for(int i=0;i<psL.size();i++){
+	        		psL.get(i).executeUpdate();
+	        	}
+				
+                conn.commit();
+
+                for(int i=0;i<psL.size();i++){
+            		psL.get(i).close();
+            	}
                 
-                try {
-                        
-                        rs=stmt.executeQuery(SQLString);
-
-                } catch (SQLException e) {
-                	e.printStackTrace();
-                	HelperClass.err(e);
-                }
-                return rs;
-        }
-
-        //Methode für alles auÃŸer select Operationen
-        public int execute(String SQLString) throws SQLException
-        {
-        	
-        		//getlist
-        	
-        		//pstmt = conn.prepareStatement();
-        	//http://download.oracle.com/javase/tutorial/jdbc/basics/transactions.html
-        	
-                int i=-1;
+                conn.setAutoCommit(true);
+	        	
+                conn.close();
                 
-                try {
-                        
-                        i=stmt.executeUpdate(SQLString);
-
-                } catch (SQLException e) {
-                	System.out.println("DEBUGSQL:["+SQLString+"]");
-                	e.printStackTrace();
-                	HelperClass.err(e);
-                }
-                return i;
-        }
-        
-    public Connection getConnection() {
-        return conn;
-    }
-
+			} catch (ClassNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+		}
         
 }
