c939520cb610c730f2f75048ae9b3fda665bb166
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-29 14:45:58 +0000

    FIX: -removed timestamp

--- src/org/dh/usertrack/DNSHelperClass.java
+++ src/org/dh/usertrack/DNSHelperClass.java
@@ -11,7 +11,6 @@ public class DNSHelperClass {
 		
 		DNSServer.add("151.10.136.202");
 		DNSServer.add("151.10.136.70");
-		//long time1=System.currentTimeMillis();
 		
 		while(sPuffer.contains("NODNS")&&i<DNSServer.size())
 		{
@@ -20,13 +19,6 @@ public class DNSHelperClass {
 			i++;
 		}
 		
-		long time2=System.currentTimeMillis();
-		
-//		if((time2-time1)>90)
-//		{
-//		System.out.println("DNS resovle took "+(time2-time1)+"ms for H["+IP+"]");
-//		}
-
 		if(sPuffer.contains("NODNS"))
 		{
 			sPuffer="";
