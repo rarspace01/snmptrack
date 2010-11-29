c5d5d4074df6b293c2ccc1a0069ca2d8d1eb2318
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-29 07:53:34 +0000

    *ADD: -OID class for better handling/customization

---
+++ trunk/src/org/dh/usertrack/OID.java
@@ -0,0 +1,20 @@
+package org.dh.usertrack;
+
+public class OID {
+
+	//IP-MIB RFC1213
+	public static final String ipNetToMediaPhysAddress="1.3.6.1.2.1.4.22.1.2";
+	public static final String sysDescr0="1.3.6.1.2.1.1.1.0";
+	public static final String sysLocation0="1.3.6.1.2.1.1.6.0";
+	public static final String sysUpTimeInstance="1.3.6.1.2.1.1.3.0";
+	public static final String sysName0="1.3.6.1.2.1.1.5.0";
+	public static final String ifPhysAddress="1.3.6.1.2.1.2.2.1.6";
+	public static final String ifOperStatus="1.3.6.1.2.1.2.2.1.8";
+	public static final String ifName="1.3.6.1.2.1.31.1.1.1.1";
+	public static final String ifAlias="1.3.6.1.2.1.31.1.1.1.18";
+	
+	//CISCO-VLAN-MEMBERSHIP-MIB - http://www.oidview.com/mibs/9/CISCO-VLAN-MEMBERSHIP-MIB.html
+	
+	public static final String vmVlan="1.3.6.1.4.1.9.9.68.1.2.2.1.2";
+	
+}
