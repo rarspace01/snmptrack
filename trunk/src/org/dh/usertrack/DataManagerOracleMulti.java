85ec81c7f0f09894f1020ca5d1cbd8680fd641b1
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-11-30 07:14:08 +0000

    *FIX: -removed unused imports

--- trunk/src/org/dh/usertrack/DataManagerOracleMulti.java
+++ trunk/src/org/dh/usertrack/DataManagerOracleMulti.java
@@ -3,11 +3,8 @@ package org.dh.usertrack;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 
 /**Die Klasse DataManager dient zum Verwalten der Verbindungen mit der Datenbank*/
