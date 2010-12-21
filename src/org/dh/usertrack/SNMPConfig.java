5d755eb5e8f4c6a30f5cf272b899578bec05862b
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2010-12-21 07:39:40 +0000

    *ADD: -2 new routers for more ARP caches

--- src/org/dh/usertrack/SNMPConfig.java
+++ src/org/dh/usertrack/SNMPConfig.java
@@ -29,6 +29,8 @@ public class SNMPConfig {
 		lRouter.add("151.10.132.2!pdhoechst");
 		//lRouter.add("151.10.97.65!pirvienna");
 		lRouter.add("151.10.143.1!pdhoechst");
+		lRouter.add("10.100.1.1!pdhoechst");
+		lRouter.add("151.10.231.1!dcsmerzig");
 		
 		return lRouter;
 	}
