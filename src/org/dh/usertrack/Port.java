da5d6724acfa3c070846b8fe4819e1a61ad6fdcd
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2011-01-18 14:50:49 +0000

    *FIX: -reduced code

--- src/org/dh/usertrack/Port.java
+++ src/org/dh/usertrack/Port.java
@@ -72,46 +72,4 @@ public class Port {
 		return sSQL;
 	}
 	
-	public void saveinDB(){
-		String svlan=vlan;
-		
-		if(svlan.length()<1){
-			svlan="-1";
-		}
-		
-		
-		
-		String sSQL="";
-		
-		int iTimestamp=(int)(System.currentTimeMillis()/1000);
-		
-		sSQL="MERGE INTO USRTRACK.\"st_ports\" dest "+
-		"USING dual ON (dual.dummy is not null and dest.MAC='"+sMAC+"' and dest.\"vlan\"="+svlan+") "+
-		"WHEN MATCHED "+
-		" THEN UPDATE SET "+
-		"\"SwitchIP\"='"+SwitchIP+"',"+
-		"\"name\"='"+name+"',"+
-		"\"alias\"='"+alias+"',"+
-		"\"stamptime\"='"+iTimestamp+"',"+
-		"\"cstatus\"='"+cstatus+"',"+
-		"\"Speed\"='"+Speed+"',"+
-		"\"Duplex\"='"+Duplex+"',"+
-		"\"isUplink\"='"+isUplink+"',"+
-		"\"UplinkIP\"='"+UplinkIP+"',"+
-		"\"PortID\"='"+PortID+"',"+
-		"\"VPortID\"='"+VPortID+"' "+
-		"WHEN NOT MATCHED "+
-		 "THEN INSERT (\"MAC\",\"vlan\",\"SwitchIP\",\"name\",\"alias\",\"stamptime\",\"cstatus\",\"Speed\",\"Duplex\",\"isUplink\",\"UplinkIP\",\"PortID\",\"VPortID\") VALUES ('"+sMAC+"',"+svlan+",'"+SwitchIP+"','"+name+"','"+alias+"','"+iTimestamp+"','"+cstatus+"','"+Speed+"','"+Duplex+"','"+isUplink+"','"+UplinkIP+"','"+PortID+"',"+VPortID+")";
-
-		try {
-			
-			DataManagerOracle.getInstance().execute(sSQL);
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
 }
