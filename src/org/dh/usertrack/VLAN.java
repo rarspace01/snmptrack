c8057356c71de6dc2e4f9136f6e1ed8a3f5f599d
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2011-01-19 08:59:46 +0000

    *FIX: -vlan class

--- src/org/dh/usertrack/VLAN.java
+++ src/org/dh/usertrack/VLAN.java
@@ -2,7 +2,7 @@ package org.dh.usertrack;
 
 public class VLAN {
 
-	public String sID, sStatus, sType, sName;
+	public String sID, sStatus, sType, sName, sVTPD;
 	
 	public String getDBString(){
 	
@@ -10,23 +10,17 @@ public class VLAN {
 		
 		int iTimestamp=(int)(System.currentTimeMillis()/1000);
 		
-//		sSQL="MERGE INTO USRTRACK.\"st_ports\" dest "+
-//		"USING dual ON (dual.dummy is not null and dest.MAC='"+sMAC+"' and dest.\"vlan\"="+svlan+") "+
-//		"WHEN MATCHED "+
-//		" THEN UPDATE SET "+
-//		"\"SwitchIP\"='"+SwitchIP+"',"+
-//		"\"name\"='"+name+"',"+
-//		"\"alias\"='"+alias+"',"+
-//		"\"stamptime\"='"+iTimestamp+"',"+
-//		"\"cstatus\"='"+cstatus+"',"+
-//		"\"Speed\"='"+Speed+"',"+
-//		"\"Duplex\"='"+Duplex+"',"+
-//		"\"isUplink\"='"+isUplink+"',"+
-//		"\"UplinkIP\"='"+UplinkIP+"',"+
-//		"\"PortID\"='"+PortID+"',"+
-//		"\"VPortID\"='"+VPortID+"' "+
-//		"WHEN NOT MATCHED "+
-//		 "THEN INSERT (\"MAC\",\"vlan\",\"SwitchIP\",\"name\",\"alias\",\"stamptime\",\"cstatus\",\"Speed\",\"Duplex\",\"isUplink\",\"UplinkIP\",\"PortID\",\"VPortID\") VALUES ('"+sMAC+"',"+svlan+",'"+SwitchIP+"','"+name+"','"+alias+"','"+iTimestamp+"','"+cstatus+"','"+Speed+"','"+Duplex+"','"+isUplink+"','"+UplinkIP+"','"+PortID+"',"+VPortID+")";
+		sSQL="MERGE INTO USRTRACK.\"st_vlans\" dest "+
+		"USING dual ON (dual.dummy is not null and dest.VLANID='"+sID+"') "+
+		"WHEN MATCHED "+
+		" THEN UPDATE SET "+
+		"STATUS='"+sStatus+"',"+
+		"TYP='"+sType+"',"+
+		"VNAME='"+sName+"',"+
+		"VTPD='"+sVTPD+"', "+
+		"STAMPTIME='"+iTimestamp+"' "+
+		"WHEN NOT MATCHED "+
+		 "THEN INSERT (VLANID,STATUS,TYP,VNAME,VTPD,STAMPTIME) VALUES ('"+sID+"','"+sStatus+"','"+sType+"','"+sName+"','"+sVTPD+"','"+iTimestamp+"')";
 
 		return sSQL;
 	}
