c0e7bc5aec1cfecdd3c79e420598e4d65223e465
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2011-01-19 11:05:42 +0000

    *ADD: -nagios get switch + readcommunity

--- trunk/src/org/dh/usertrack/Nagios.java
+++ trunk/src/org/dh/usertrack/Nagios.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 public class Nagios {
 
+	static ArrayList<Switch> switchList=new ArrayList<Switch>();
+	
 	public static final ArrayList<String> getHostsOfGroupOverHTTP(String sGroup){
 		
 		ArrayList<String> hostList=new ArrayList<String>();
@@ -60,12 +62,27 @@ public class Nagios {
 		return hostList;
 	}
 	
-	public static final ArrayList<String> getSwitchs(){
+	public static final ArrayList<Switch> getSwitchs(){
 		ArrayList<String> hostList=new ArrayList<String>();
+		ArrayList<String> hostListrc=new ArrayList<String>();
 		
-		String sSQL="SELECT h.address AS address FROM `tbl_hostgroup` hg, `tbl_lnkHostgroupToHost` hg2h, `tbl_host` h WHERE hg.id=hg2h.idMaster AND hg2h.idSlave=h.id";
+		String sSQL="SELECT h.address, vd.value FROM tbl_host h, tbl_lnkHostToHosttemplate h2ht,tbl_lnkHosttemplateToVariabledefinition ht2vd, tbl_variabledefinition vd WHERE h.id=h2ht.idMaster AND h2ht.idSlave=ht2vd.idMaster AND ht2vd.idSlave=vd.id AND h.active='1' AND vd.name LIKE '_readcommunity' ORDER BY address;";
 		
-		return null;
+		try {
+			ResultSet rset=DataManagerMySQL.getInstance().select(sSQL);
+			
+			while(rset.next()){
+			
+			switchList.add(new Switch(rset.getString("address"), rset.getString("value")));
+						
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return switchList;
 	}
 	
 }
