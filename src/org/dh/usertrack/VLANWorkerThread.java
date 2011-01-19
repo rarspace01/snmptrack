31a4864c5a1cbecbbc9fddf2eeec7679f6aed36f
denishamann <denishamann@afc6bf6b-8819-2fa6-ec08-acd71723b07d>
2011-01-19 09:00:20 +0000

    *FIX: -vlan thread logic

--- src/org/dh/usertrack/VLANWorkerThread.java
+++ src/org/dh/usertrack/VLANWorkerThread.java
@@ -7,6 +7,9 @@ import org.snmp4j.TransportMapping;
 import org.snmp4j.smi.Address;
 
 class VLANWorkerThread implements Runnable {
+	
+	ArrayList<VLAN> vlList=new ArrayList<VLAN>();
+	
 	Thread runner;
 	Address targetAddress;
 	String sIP;
@@ -29,9 +32,13 @@ class VLANWorkerThread implements Runnable {
 
 	  public void run() {
 
+		  String sID="",sName="",sTyp="", sStatus="", sVTP;
+		  
 		  ArrayList<String> svStatus=new ArrayList<String>();
 		  ArrayList<String> svTyp=new ArrayList<String>();
 		  ArrayList<String> svName=new ArrayList<String>();
+		  
+		  ArrayList<String> sSQLlist=new ArrayList<String>();
 		  
 		//Switch workerVLAN=new Switch(sIP, sReadc, this.snmp);
 		
@@ -43,7 +50,45 @@ class VLANWorkerThread implements Runnable {
 		  
 		  svName=SNMPHandler.getOIDWalknonBulkSlow(snmp, OIDL.vtpVlanName, SNMPConfig.getRouters().get(0).substring(0,SNMPConfig.getRouters().get(0).indexOf("!")), SNMPConfig.getRouters().get(0).substring(SNMPConfig.getRouters().get(0).indexOf("!")+1));
 
+		  for(int i=0;i<svName.size();i++){
+			  
+			  VLAN vl=new VLAN();
+			  
+			  sID=svName.get(i).substring(OIDL.vtpVlanName.length()+1, svName.get(i).indexOf("!"));	  
+			  sName=svName.get(i).substring(svName.get(i).indexOf("!")+1);
+			  sStatus=svStatus.get(i).substring(svStatus.get(i).indexOf("!")+1);
+			  sTyp=svTyp.get(i).substring(svTyp.get(i).indexOf("!")+1);
+			  
+			  //System.out.println(sID);
+			  
+			  sVTP=sID.substring(0,sID.indexOf("."));
+			  
+			  sID=sID.substring(sID.indexOf(".")+1);
+			  
+			  vl.sID=sID;
+			  vl.sVTPD=sVTP;
+			  vl.sName=sName;
+			  vl.sStatus=sStatus;
+			  vl.sType=sTyp;
+			  
+			  vlList.add(vl);
+			  
+		  }
+		  
+		  for(int j=0;j<vlList.size();j++){
+			 
+			  sSQLlist.add(vlList.get(j).getDBString());
+			  
+		  }
+		  
+		  DBComitterThread dbcv=new DBComitterThread("VLANS", sSQLlist);
+		  
 		 //erstelle VLANs 
+		  
+			//sPufferList=SNMPHandler.getOIDWalknonBulk(snmp, , SNMPConfig.getRouters().get(0).substring(0,SNMPConfig.getRouters().get(0).indexOf("!")), SNMPConfig.getRouters().get(0).substring(SNMPConfig.getRouters().get(0).indexOf("!")+1));
+			
+			//SNMPConfig.getRouters().get(0).substring(0,SNMPConfig.getRouters().get(0).indexOf("!"));
+			
 		  
 	  }
 	  
